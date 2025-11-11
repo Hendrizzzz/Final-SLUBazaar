e2b2c5df1cbd446aa0240e5cb648341f5dc0e7de
Hendrizzzz <jimhendrixbageo@gmail.com>
2025-11-11 18:54:55 +0800

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'your-name/final-slubazaar',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7e51c7ffbab93e81aad3289d568fd112e0e997ae',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'your-name/final-slubazaar' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7e51c7ffbab93e81aad3289d568fd112e0e997ae',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
